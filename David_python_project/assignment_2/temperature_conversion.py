39dbece8eb679ad60ac255e3a27b4c2b1a899754
David <osodavid001@gmail.com>
2022-10-24 15:27:51 +0100

    monday commit

--- David_python_project/assignment_2/temperature_conversion.py
+++ David_python_project/assignment_2/temperature_conversion.py
@@ -1,1 +1,14 @@
-def celsius():
+def kelvin(temp1):
+    return round(temp1 + 273.15, 2)
+
+
+def celsius(temp2):
+    return round(temp2 - 273.15, 2)
+
+
+if __name__ == '__main__':
+    temperature1 = float(input("Enter the temperature in celsius: "))
+    print("Temperature in kelvin is:", kelvin(temperature1))
+
+    temperature2 = float(input("Enter the temperature in kelvin: "))
+    print("Temperature in celsius is:", celsius(temperature2))
